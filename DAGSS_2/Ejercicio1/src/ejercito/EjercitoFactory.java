86da3e03872e2fd98dc82319f9a0134bd6858e24
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-10-16 14:23:26 +0200

    Ejercicio 2_1: Corregido

--- DAGSS_2/Ejercicio1/src/ejercito/EjercitoFactory.java
+++ DAGSS_2/Ejercicio1/src/ejercito/EjercitoFactory.java
@@ -5,6 +5,14 @@ import soldados.Zapador;
 
 public interface EjercitoFactory {
 
+	public static EjercitoFactory getEjercito(String tipo) {
+		if (tipo.equals("Harkonen")) {
+			return new HarkonenFactory();
+		} else {
+			return new AtreidesFactory();
+		}
+	}
+
 	public Zapador crearZapador();
 
 	public Artillero crearArtillero();
--- DAGSS_2/Ejercicio1/src/jugador/Jugador.java
+++ DAGSS_2/Ejercicio1/src/jugador/Jugador.java
@@ -3,9 +3,7 @@ package jugador;
 import java.util.LinkedList;
 import java.util.List;
 
-import ejercito.AtreidesFactory;
 import ejercito.EjercitoFactory;
-import ejercito.HarkonenFactory;
 import soldados.Artillero;
 import soldados.Zapador;
 
@@ -13,21 +11,13 @@ public abstract class Jugador {
 
 	protected List<Artillero> artilleros = new LinkedList<Artillero>();
 	protected List<Zapador> zapadores = new LinkedList<Zapador>();
-	protected String tipo;
 	protected EjercitoFactory ejercitoFactory;
 
 	public Jugador(String tipo) {
-		this.tipo = tipo;
 
 		// creamos el ejercito inicial (todos de la misma raza, no se pueden mezclar!)
-
-		if (tipo.equals("Harkonen")) {
-			this.ejercitoFactory = new HarkonenFactory();
-
-		} else if (tipo.equals("Atreides")) {
-			this.ejercitoFactory = new AtreidesFactory();
-		}
-
+		this.ejercitoFactory = EjercitoFactory.getEjercito(tipo);
+		
 		artilleros.add(this.ejercitoFactory.crearArtillero());
 		artilleros.add(this.ejercitoFactory.crearArtillero());
 		zapadores.add(this.ejercitoFactory.crearZapador());
