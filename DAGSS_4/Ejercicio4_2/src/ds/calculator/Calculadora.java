d095c787012530a980fc2d1812bb27683e6b0600
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-11-03 18:21:02 +0100

    Rename package

--- DAGSS_4/Ejercicio4_2/src/ds/calculator/Calculadora.java
+++ DAGSS_4/Ejercicio4_2/src/ds/calculator/Calculadora.java
@@ -1,4 +1,4 @@
-package calculadora;
+package ds.calculator;
 
 import java.util.LinkedList;
 import java.util.List;
--- DAGSS_4/Ejercicio4_2/src/ds/calculator/CalculaduraAPP.java
+++ DAGSS_4/Ejercicio4_2/src/ds/calculator/CalculaduraAPP.java
@@ -1,4 +1,4 @@
-package calculadora;
+package ds.calculator;
 
 public class CalculaduraAPP {
 
