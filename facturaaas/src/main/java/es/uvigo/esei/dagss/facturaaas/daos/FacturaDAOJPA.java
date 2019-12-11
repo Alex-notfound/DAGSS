b7a945f321ed2ed642df3db6d7c91046881fe60c
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-12-12 00:50:37 +0100

    Implementado FacturaDAO

--- facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/daos/FacturaDAOJPA.java
+++ facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/daos/FacturaDAOJPA.java
@@ -5,8 +5,25 @@
  */
 package es.uvigo.esei.dagss.facturaaas.daos;
 
+import es.uvigo.esei.dagss.facturaaas.entidades.Cliente;
 import es.uvigo.esei.dagss.facturaaas.entidades.Factura;
+import es.uvigo.esei.dagss.facturaaas.entidades.Usuario;
+import java.util.List;
+import javax.persistence.TypedQuery;
 
 public class FacturaDAOJPA extends GenericoDAOJPA<Factura, Long> implements FacturaDAO {
 
+    @Override
+    public List<Factura> buscarPorUsuario(Usuario usuario) {
+        TypedQuery<Factura> query = em.createQuery("SELECT f FROM Factura AS f WHERE f.usuario.id = :usuarioId", Factura.class);
+        query.setParameter("usuarioId", usuario.getId());
+        return query.getResultList();
+    }
+    
+    @Override
+    public List<Factura> buscarPorCliente(Cliente cliente) {
+        TypedQuery<Factura> query = em.createQuery("SELECT f FROM Factura AS f WHERE f.cliente.id = :clienteId", Factura.class);
+        query.setParameter("clienteId", cliente.getId());
+        return query.getResultList();
+    }
 }
