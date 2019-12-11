90a7e89b0ef7eeec202da4ad57d484ccca8cca62
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-12-12 00:34:43 +0100

    Create FacturasController.java

---
+++ facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/controladores/usuario/FacturasController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uvigo.esei.dagss.facturaaas.controladores.usuario;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named(value = "facturasController")
+@ViewScoped
+public class FacturasController implements Serializable {
+
+}
