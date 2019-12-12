e3e3e626c28bd9e994f1931b233ddd9be2dc5dac
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2019-12-12 01:01:13 +0100

    Implementado PagoDAO

--- facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/daos/PagoDAO.java
+++ facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/daos/PagoDAO.java
@@ -5,8 +5,16 @@
  */
 package es.uvigo.esei.dagss.facturaaas.daos;
 
+import es.uvigo.esei.dagss.facturaaas.entidades.Cliente;
+import es.uvigo.esei.dagss.facturaaas.entidades.Factura;
 import es.uvigo.esei.dagss.facturaaas.entidades.Pago;
+import es.uvigo.esei.dagss.facturaaas.entidades.Usuario;
+import java.util.List;
 
 public interface PagoDAO extends GenericoDAO<Pago, Long> {
 
+    public List<Pago> buscarPorUsuario(Usuario usuario);
+
+    public List<Pago> buscarPorCliente(Cliente cliente);
+    
 }
--- facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/daos/PagoDAOJPA.java
+++ facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/daos/PagoDAOJPA.java
@@ -5,8 +5,25 @@
  */
 package es.uvigo.esei.dagss.facturaaas.daos;
 
+import es.uvigo.esei.dagss.facturaaas.entidades.Cliente;
 import es.uvigo.esei.dagss.facturaaas.entidades.Pago;
+import es.uvigo.esei.dagss.facturaaas.entidades.Usuario;
+import java.util.List;
+import javax.persistence.TypedQuery;
 
 public class PagoDAOJPA extends GenericoDAOJPA<Pago, Long> implements PagoDAO {
 
+    @Override
+    public List<Pago> buscarPorUsuario(Usuario usuario) {
+        TypedQuery<Pago> query = em.createQuery("SELECT p FROM Pago AS p WHERE p.usuario.id = :usuarioId", Pago.class);
+        query.setParameter("usuarioId", usuario.getId());
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Pago> buscarPorCliente(Cliente cliente) {
+        TypedQuery<Pago> query = em.createQuery("SELECT p FROM Pago AS p WHERE p.cliente.id = :clienteId", Pago.class);
+        query.setParameter("clienteId", cliente.getId());
+        return query.getResultList();
+    }
 }
