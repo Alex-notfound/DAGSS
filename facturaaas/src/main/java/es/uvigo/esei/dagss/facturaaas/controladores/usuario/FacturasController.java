ee86b5dbb39df6bc94834d48c92deb1d0d6b6595
Alexandre Currás Rodríguez <alexcurrasr@gmail.com>
2020-01-15 01:47:46 +0100

    Fixed: TipoIVA por defecto, calculo importes, supresion de IVA en Factura y arreglo de importe de Pagos

--- facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/controladores/usuario/FacturasController.java
+++ facturaaas/src/main/java/es/uvigo/esei/dagss/facturaaas/controladores/usuario/FacturasController.java
@@ -36,6 +36,7 @@ import javax.inject.Named;
 @Named(value = "facturasController")
 @ViewScoped
 public class FacturasController implements Serializable {
+
     private List<Factura> facturas;
     private Factura facturaActual;
     private LineaFactura lineaFacturaActual;
@@ -44,27 +45,27 @@ public class FacturasController implements Serializable {
     private List<Cliente> clientesFacturas;
     private Cliente clienteFiltrado;
     private DatosFacturacion datosFacturacionUsuario;
-    
+
     @Inject
     private FacturaDAO daoFactura;
-    
+
     @Inject
     private LineaFacturaDAO daoLineaFactura;
-    
+
     @Inject
     private PagoDAO daoPago;
-    
+
     @Inject
     private DatosFacturacionDAO daoDatosFacturacion;
-    
+
     @Inject
     private AutenticacionController authController;
-    
-    public List<Factura> getFacturas(){
+
+    public List<Factura> getFacturas() {
         return facturas;
     }
-    
-    public void setFactura(List<Factura> facturas){
+
+    public void setFactura(List<Factura> facturas) {
         this.facturas = facturas;
     }
 
@@ -83,7 +84,7 @@ public class FacturasController implements Serializable {
     public void setLineaFacturaActual(LineaFactura lineaFacturaActual) {
         this.lineaFacturaActual = lineaFacturaActual;
     }
-    
+
     public boolean isEsNuevo() {
         return esNuevo;
     }
@@ -99,7 +100,7 @@ public class FacturasController implements Serializable {
     public void setClientesFacturas(List<Cliente> clienteFiltrado) {
         this.clientesFacturas = clienteFiltrado;
     }
-    
+
     public Cliente getClienteFiltrado() {
         return clienteFiltrado;
     }
@@ -107,134 +108,138 @@ public class FacturasController implements Serializable {
     public void setClienteFiltrado(Cliente clienteFiltrado) {
         this.clienteFiltrado = clienteFiltrado;
     }
-    
-    public EstadoFactura[] getEstadosFactura(){
+
+    public EstadoFactura[] getEstadosFactura() {
         return EstadoFactura.values();
     }
 
     @PostConstruct
-    public void cargaInicial(){
+    public void cargaInicial() {
         this.facturas = refrescarListaFacturas();
         this.clientesFacturas = obtenerClientes();
         this.facturaActual = null;
         this.esNuevo = false;
         this.lineasEliminadas = new ArrayList<>();
-        
+
         this.datosFacturacionUsuario = daoDatosFacturacion.buscarConPropietario(authController.getUsuarioLogueado());
     }
-    
-    public void doBuscarPorCliente(){
-        if(this.clienteFiltrado != null){
+
+    public void doBuscarPorCliente() {
+        if (this.clienteFiltrado != null) {
             this.facturas = daoFactura.buscarPorCliente(clienteFiltrado);
-        }else{
+        } else {
             this.facturas = this.refrescarListaFacturas();
         }
     }
-    
-    
-    
-    public void doNuevo(){
+
+    public void doNuevo() {
         this.esNuevo = true;
         this.facturaActual = new Factura();
-        this.facturaActual.setFormaPago(datosFacturacionUsuario.getFormaPagoPorDefecto());
-        
         this.lineaFacturaActual = new LineaFactura();
-        this.lineaFacturaActual.setIva(datosFacturacionUsuario.getTipoIVAPorDefecto());
         
+        if (datosFacturacionUsuario != null) {
+            if (datosFacturacionUsuario.getFormaPagoPorDefecto() != null) {
+                this.facturaActual.setFormaPago(datosFacturacionUsuario.getFormaPagoPorDefecto());
+            }
+            if (datosFacturacionUsuario.getTipoIVAPorDefecto() != null) {
+                this.lineaFacturaActual.setIva(datosFacturacionUsuario.getTipoIVAPorDefecto());
+            }
+        }
+
         this.facturaActual.setUsuario(authController.getUsuarioLogueado());
         this.facturaActual.setLineasFactura(refrescarListaLineasFacturas());
     }
-    
-    public void doEditar(Factura factura){
+
+    public void doEditar(Factura factura) {
         this.esNuevo = false;
         this.facturaActual = factura;
         this.facturaActual.setLineasFactura(refrescarListaLineasFacturas());
         this.lineaFacturaActual = new LineaFactura();
     }
-    
-    public void doCrearLineaVenta(){
-        
+
+    public void doCrearLineaVenta() {
+
         Boolean errors = false;
-        
-        if(this.lineaFacturaActual.getConcepto().length() < 1){
+
+        if (this.lineaFacturaActual.getConcepto().length() < 1) {
             errors = true;
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR ,
-                                                                                "El concepto no puede estar vacio", 
-                                                                                null));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "El concepto no puede estar vacio",
+                    null));
         }
-        
-        try{
+
+        try {
             Integer integer = (Integer) this.lineaFacturaActual.getCantidad();
-        }catch(NullPointerException exc){
+        } catch (NullPointerException exc) {
             errors = true;
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR ,
-                                                                                "El campo cantidad tiene que tener un valor entero", 
-                                                                                null));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "El campo cantidad tiene que tener un valor entero",
+                    null));
         }
-        
-        try{
+
+        try {
             Long l = (long) this.lineaFacturaActual.getPrecio();
-        }catch(NullPointerException exc){
+        } catch (NullPointerException exc) {
             errors = true;
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR ,
-                                                                                "El campo precio tiene que tener un valor numerico", 
-                                                                                null));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "El campo precio tiene que tener un valor numerico",
+                    null));
         }
-        
-        try{
+
+        try {
             Long l = (long) this.lineaFacturaActual.getDescuento();
-        }catch(NullPointerException exc){
+        } catch (NullPointerException exc) {
             errors = true;
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR ,
-                                                                                "El campo descuento tiene que tener un valor numerico", 
-                                                                                null));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "El campo descuento tiene que tener un valor numerico",
+                    null));
         }
-        
-        if(this.lineaFacturaActual.getIva() == null){
+
+        if (this.lineaFacturaActual.getIva() == null) {
             errors = true;
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR ,
-                                                                                "Tiene que selecciona un tipo de IVA", 
-                                                                                null));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Tiene que selecciona un tipo de IVA",
+                    null));
         }
-        
-        if(!errors){
+
+        if (!errors) {
             this.lineaFacturaActual.setFactura(this.facturaActual);
             this.facturaActual.addLineaFactura(this.lineaFacturaActual);
             this.lineaFacturaActual = new LineaFactura();
         }
-        
+
     }
-    
-    public void doEditarLineaVenta(LineaFactura linea){
+
+    public void doEditarLineaVenta(LineaFactura linea) {
         this.lineaFacturaActual = linea;
     }
-    
-    public void doEliminarLineaVenta(LineaFactura linea){
+
+    public void doEliminarLineaVenta(LineaFactura linea) {
         this.facturaActual.deleteLineaFactura(linea);
         this.lineasEliminadas.add(linea);
     }
-    
-    public void doGuardarEditado(){
-        if(this.esNuevo){
+
+    public void doGuardarEditado() {
+        if (this.esNuevo) {
             daoFactura.crear(facturaActual);
-        }else{
+        } else {
             daoFactura.actualizar(facturaActual);
             this.borrarPagos();
         }
-        
+
         this.crearPagos();
-        
-        for(LineaFactura lf: this.lineasEliminadas){
+
+        for (LineaFactura lf : this.lineasEliminadas) {
             this.daoLineaFactura.eliminar(lf);
         }
-        
+
         this.facturas = refrescarListaFacturas();
         //this.clientesFacturas = obtenerClientes();
         this.facturaActual = null;
         this.esNuevo = false;
     }
-    
-    public void doCancelarEditado(){
+
+    public void doCancelarEditado() {
         this.facturaActual = null;
         this.esNuevo = false;
         this.lineasEliminadas.clear();
@@ -243,49 +248,49 @@ public class FacturasController implements Serializable {
     private List<Factura> refrescarListaFacturas() {
         return daoFactura.buscarPorUsuario(authController.getUsuarioLogueado());
     }
-    
+
     private List<LineaFactura> refrescarListaLineasFacturas() {
         return daoLineaFactura.buscarPorFactura(this.facturaActual);
     }
 
-    private List<Cliente> obtenerClientes(){
+    private List<Cliente> obtenerClientes() {
         List<Cliente> listaClientes = new ArrayList<>();
-        for(Factura f : this.facturas){
-            if(!listaClientes.contains(f.getCliente())){
+        for (Factura f : this.facturas) {
+            if (!listaClientes.contains(f.getCliente())) {
                 listaClientes.add(f.getCliente());
             }
         }
-        
+
         return listaClientes;
     }
-    
-    private void crearPagos(){
-        
+
+    private void crearPagos() {
+
         int periodicidad = facturaActual.getFormaPago().getPeriodicidad();
         Date fecha = facturaActual.getFechaEmision();
         Calendar c = Calendar.getInstance();
         c.setTime(fecha);
         //Añade dias a la fecha
         c.add(Calendar.DATE, periodicidad);
-        
+
         for (int i = 0; i < this.facturaActual.getFormaPago().getNumeroPagos(); i++) {
-            Pago pago = new Pago();         
+            Pago pago = new Pago();
             pago.setCliente(this.facturaActual.getCliente());
             pago.setEstado(EstadoPago.PENDIENTE);
             pago.setFactura(facturaActual);
-            pago.setImporte(this.facturaActual.getImporte());
+            pago.setImporte(this.facturaActual.getImporte() / this.facturaActual.getFormaPago().getNumeroPagos());
             pago.setNombre(this.facturaActual.getFormaPago().getNombre());
             pago.setUsuario(authController.getUsuarioLogueado());
             pago.setFecha(c.getTime());
             this.daoPago.crear(pago);
-            
+
             c.add(Calendar.DATE, periodicidad);
         }
-        
+
     }
-    
-    private void borrarPagos(){
-        for(Pago p: this.daoPago.buscarPorFactura(facturaActual)){
+
+    private void borrarPagos() {
+        for (Pago p : this.daoPago.buscarPorFactura(facturaActual)) {
             this.daoPago.eliminar(p);
         }
     }
